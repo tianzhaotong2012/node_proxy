25147ed06e6dbd21e7070c8a0449e1a8c882a7da
Your Name <you@example.com>
2017-12-07 05:20:19 -0800

    change

--- site_conf/www_zrmm_com.js
+++ site_conf/www_zrmm_com.js
@@ -9,7 +9,7 @@ var handle = function(body) {
 				$(e).attr("href",afterHref).html();	
 		}
    });
-   $(".conpage").children().first().html("<p></p>");
+   //$(".conpage").children().first().html("<p></p>");
    return $.html();
 };
 
