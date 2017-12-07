bdbde7c526523e73cd9c603de700fc3136f0c3fd
Your Name <you@example.com>
2017-12-07 04:39:46 -0800

    first commit

---
+++ proxy.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
+app.use(function(req, res) {
+    var url = req.url;
+    request(url, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+		var headers = response.headers;
+		var type = headers['content-type'];
+		if(typeof(type)!="undefined" && type.indexOf("text/html")>=0){			
+			var host = 	req.headers['host'];
+			if(host == "www.zrmm.com"){
+				var zrmm = require("./site_conf/www_zrmm_com.js");
+				newBody = zrmm.handle(body);
+				res.send(newBody);
+				return;
+			}
+			if(host == "www.zhisland.com"){
+				var zhisland = require("./site_conf/www_zhisland_com.js");
+				newBody = zhisland.handle(body);
+				res.send(newBody);
+				return;
+			}
+			req.pipe(request(url)).pipe(res);	
+		}else{
+			req.pipe(request(url)).pipe(res);
+		}
+	  }
+    });
+});
+app.listen(process.env.PORT || 9003);
---
+++ site_conf/www_zrmm_com.js
@@ -0,0 +1,15 @@
+var cheerio = require('cheerio');
+
+var handle = function(body) {
+  const $ = cheerio.load(body);
+  $("a").each(function(i, e) {
+		var href = $(e).attr("href");
+		if(typeof(href)!="undefined" && $(e).attr("href").indexOf("http")<0){
+				var afterHref = "http://www.zrmm.com" + $(e).attr("href");
+				$(e).attr("href",afterHref).html();	
+		}
+   });
+   return $.html();
+};
+
+module.exports.handle = handle;
