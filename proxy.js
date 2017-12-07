989bc5a37a1bb92987a04eedb10f47f07e14a908
Your Name <you@example.com>
2017-12-07 06:14:22 -0800

    change head

--- proxy.js
+++ proxy.js
@@ -13,8 +13,9 @@ app.use(function(req, res) {
 			if(host == "www.zrmm.com"){
 				var zrmm = require("./site_conf/www_zrmm_com.js");
 				newBody = zrmm.handle(body);
-				res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
-				res.end(newBody);
+				res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});  
+    // 发送响应数据 "Hello World"  
+       			res.write(newBody); 
 				return;
 			}
 			if(host == "www.zhisland.com"){
