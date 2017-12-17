438e9eb588094c7a1c96f1949b78e52dba195448
Your Name <you@example.com>
2017-12-16 20:31:27 -0800

    fix

--- site_conf/mil_sina_cn.js
+++ site_conf/mil_sina_cn.js
@@ -4,6 +4,7 @@ var handle = function(url) {
   var execStr = './phantomjs/bin/phantomjs ./phantomjs/js/get_html.js ' + url + ' t.png' + ' 1080*1920';
   var ret = shell.exec(execStr);
   if(ret.code !== 0){
+	console.log(execStr);
 	return 'exec run';
    }
   var data = ret.stdout; 
