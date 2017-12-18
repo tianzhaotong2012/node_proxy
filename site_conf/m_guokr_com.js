0a8a2062216bf16cbddc5d0465efe3fc49181370
Your Name <you@example.com>
2017-12-18 04:16:45 -0800

    fix

--- site_conf/m_guokr_com.js
+++ site_conf/m_guokr_com.js
@@ -3,7 +3,7 @@ var cheerio = require('cheerio');
 
 var handle = function(url) {
   if(url == "http://m.guokr.com/scientific/"){
-	var execStr = "curl https://m.guokr.com/apis/minisite/article.json?retrieve_type=by_subject_v2&channel_key=&subject_key=all&limit=20&offset=0";
+	var execStr = "curl -H 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36' https://m.guokr.com/apis/minisite/article.json?retrieve_type=by_subject_v2&channel_key=&subject_key=all&limit=20&offset=0";
       var ret = shell.exec(execStr);
 	if(ret.code !== 0){
 		console.log(execStr);
@@ -12,9 +12,9 @@ var handle = function(url) {
 		var str = "";
 	  	var obj = eval('(' + ret.stdout + ')');
 	  	for(var item in obj.result) {
-			str += "<a href='";
-			str += obj.result[item].url;
-			str += "'>";
+			str += "<a href='https://m.guokr.com/article/";
+			str += obj.result[item].id;
+			str += "/'>";
 			str += item;
 			str += "</a><br>";
 	   	}
