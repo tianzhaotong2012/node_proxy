70bad59e1c0714bb106a489277047ff6c47c9e5d
Your Name <you@example.com>
2017-12-18 03:07:00 -0800

    fix

--- site_conf/sports_sina_cn.js
+++ site_conf/sports_sina_cn.js
@@ -1,8 +1,13 @@
-require('shelljs/global');
+var shell = require("shelljs");
 
 var handle = function(url) {
-  var execStr = './phantomjs/bin/phantomjs ./phantomjs/js/get_html.js ' + url + ' t.png' + ' 1080*1920';
-  var data = exec(execStr,{silent:true}).stdout;
+  var execStr = "./phantomjs/bin/phantomjs ./phantomjs/js/get_html.js '" + url + "' t.png" + " 1080*1920";
+  var ret = shell.exec(execStr);
+  if(ret.code !== 0){
+	console.log(execStr);
+	return 'exec run';
+   }
+  var data = ret.stdout; 
   return data
 };
 
